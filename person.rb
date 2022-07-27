7db5f58d72f6d782b953adfe55e0059ea693f9ca
kamzzy <preciousakams@yahoo.com>
2022-07-27 14:15:48 +0100

    milestone 2

--- person.rb
+++ person.rb
@@ -1,5 +1,7 @@
-class Person
+require './nameable'
+class Person < Nameable
   def initialize(age, name = 'unknown', parent_permission: true)
+    super()
     @id = rand(1..1000)
     @name = name
     @age = age
@@ -7,15 +9,19 @@ class Person
   end
 
   attr_reader :id
-  attr_accessor :name, :age
+  attr_accessor :age, :name
+
+  def can_use_services?
+    true if is_of_age || @parent_permission
+  end
+
+  def correct_name
+    @name
+  end
 
   private
 
   def of_age?
-   @age >= 18
-  end
-
-  def can_use_services?
-    true if is_of_age || @parent_permission
+    @age >= 18
   end
 end
