787d54df56ce7e7e31b989a7601d5e9ccdf1a891
kamzzy <preciousakams@yahoo.com>
2022-07-28 20:55:10 +0100

    milestone 3

--- person.rb
+++ person.rb
@@ -1,15 +1,17 @@
 require './nameable'
+# person has-many rentals
 class Person < Nameable
+  attr_reader :id, :rentals
+  attr_accessor :age, :name
+
   def initialize(age, name = 'unknown', parent_permission: true)
     super()
     @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
-
-  attr_reader :id
-  attr_accessor :age, :name
 
   def can_use_services?
     true if is_of_age || @parent_permission
@@ -17,6 +19,11 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental)
+    rental.person = self
   end
 
   private
--- student.rb
+++ student.rb
@@ -1,9 +1,13 @@
 require './person'
 
+# student belongs-to classroom
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom)
     super
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
