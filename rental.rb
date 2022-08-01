b676c6f1b4c954a3e6fd2242fa8d7a55fbcd8565
kamzzy <preciousakams@yahoo.com>
2022-08-01 19:10:52 +0100

    resolved

--- rental.rb
+++ rental.rb
@@ -1,8 +1,9 @@
 # a rental belongs-to a book and a person
+
 class Rental
   attr_accessor :date, :book, :person
 
-  def initialize(date, book, person)
+  def initialize(date, person, book)
     @date = date
     @book = book
     book.rentals << self
