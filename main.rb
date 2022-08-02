412d946c47fbf3e1920e8e6905791e23b7ab5445
kamzzy <preciousakams@yahoo.com>
2022-08-02 10:46:27 +0100

    option moved

--- main.rb
+++ main.rb
@@ -21,27 +21,4 @@ def display_list
   7 - Exit"
 end
 
-def options(books, people, rentals)
-  loop do
-    display_list
-    option = gets.chomp.to_i
-    case option
-    when 1
-      all_books(books)
-    when 2
-      all_people(people)
-    when 3
-      create_a_person(people)
-    when 4
-      create_a_book(books)
-    when 5
-      create_rental(books, people, rentals)
-    when 6
-      all_rentals_by_id(rentals)
-    else
-      break
-    end
-  end
-end
-
 main
